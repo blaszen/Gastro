b938bc897a44a32ef34119c40d60f9618c7ca0c3
Blaszen <blaszen.dev@gmail.com>
2025-11-25 22:45:44 -0800

    logout function created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import { signOut } from "firebase/auth";
+import { auth } from "../../lib/firebase";
+
 export default function TabOneScreen() {
+  const onLogout = async () => {
+    try {
+      await signOut(auth);
+      // Automatically redirects to /auth/login because of onAuthStateChanged
+    } catch (err) {
+      console.log("Logout error:", err);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <TouchableOpacity
+        onPress={onLogout}
+        style={{
+          backgroundColor: "#e53935",
+          paddingVertical: 12,
+          paddingHorizontal: 20,
+          borderRadius: 10,
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "600" }}>Log Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
